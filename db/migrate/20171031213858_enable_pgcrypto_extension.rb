7fb14cc0d6756c8ac074a277c5281d6d03e2f978
LJruby <labuda.jakub@gmail.com>
2017-11-04 17:56:30 +0100

    sob, 4 lis 2017, 17:56:30 CET

--- db/migrate/20171031213858_enable_pgcrypto_extension.rb
+++ db/migrate/20171031213858_enable_pgcrypto_extension.rb
@@ -1,10 +1,11 @@
 class EnablePgcryptoExtension < ActiveRecord::Migration[5.1]
   def create
     enable_extension 'pgcrypto'
-     create_table :reservations, id: :uuid do |t|
+    create_table :reservations, id: :uuid do |t|
       t.text :name
       t.date :from
       t.date :to
-      t.timestamps
+      t.timestamps  
+    end
   end
 end
